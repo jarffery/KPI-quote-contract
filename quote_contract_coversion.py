11b239a936ab1cde282c60d07fbca2f549c2e321
jarffery <jerrylee0128@gmail.com>
2020-04-04 21:34:37 -0700

    updates

--- quote_contract_coversion.py
+++ quote_contract_coversion.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from datetime import datetime, timedelta
 quotes = pd.read_csv("./all quotes.csv")
 contracts = pd.read_csv("./all_contracts_merged.csv")
 TS_name = quotes['TS'].drop_duplicates().values.tolist()
@@ -8,5 +8,18 @@ TS_contract_dic = dict()
 for TSname in TS_name:
     TS_quote_dic.update(
         {TSname: quotes.loc[quotes['TS'].str.contains(TSname)]})
+    TS_quote_dic[TSname]["Date"] = pd.to_datetime(TS_quote_dic[TSname]["Date"]).dt.to_period(
+        "Q")
     TS_contract_dic.update(
         {TSname: contracts.loc[contracts['TS'].str.contains(TSname)]})
+    TS_contract_dic[TSname]["Date"] = pd.to_datetime(TS_contract_dic[TSname]["Date"]).dt.to_period(
+        "Q")
+
+    try:
+        Pn = (Qn_contract_num + Qn_contract_num2*0.7)/len(Qn_quote_list)
+    except ZeroDivisionError:
+        Pn = 0
+    try:
+        Pm = (Qm_contract_num + \Qm_contract_num2*0.7)/len(Qm_quote_list)
+    except ZeroDivisionError:
+        Pm = 0
